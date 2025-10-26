4919567df356a09a4d0b6aae661c0c63c231e64f
Amey Prakash <aditiaditi737@gmail.com>
2025-10-26 15:33:01 +0530

    Program to print ASCII value

---
+++ ascii_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {  
+    char c;
+    
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+
+    printf("ASCII value of %c = %d", c, c);
+    system("pause");
+return 0;
+}
