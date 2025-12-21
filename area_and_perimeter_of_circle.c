d708a91fec514fbe3e3b4bfd9864fd07ecf0fe8d
Amey Prakash <aditiaditi737@gmail.com>
2025-12-21 18:10:08 +0530

    updated

--- area_and_perimeter_of_circle.c
+++ area_and_perimeter_of_circle.c
@@ -1,15 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Function declaration
+void calculateCircle(float radius, float *area, float *circumference);
+
 int main() {
     float radius, area, circumference;
     const float pi = 3.14159;
-    printf("Enter the radius of the circle:");
+    
+    printf("Enter the radius of the circle: ");
     scanf("%f", &radius);
-    area = pi * radius * radius;
-    circumference = 2 * pi * radius;
-    printf("area = %.2f\n", area);
-    printf("circumference = %.2f\n", circumference),
+    
+    // Call function using pointers
+    calculateCircle(radius, &area, &circumference);
+    
+    printf("Area = %.2f\n", area);
+    printf("Circumference = %.2f\n", circumference);
+    
     system("pause");
     return 0;
+}
+
+// Function definition using pointers
+void calculateCircle(float radius, float *area, float *circumference) {
+    const float pi = 3.14159;
+    *area = pi * radius * radius;
+    *circumference = 2 * pi * radius;
 }
