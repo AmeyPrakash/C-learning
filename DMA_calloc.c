5955088d0af3a26e9ed8f730fd5f311a3ceb2deb
AmeyPrakash <aditiaditi737@gmail.com>
2026-08-05 18:04:25 +0530

    calloc array and variable , heap memory stack memory

--- DMA_calloc.c
+++ DMA_calloc.c
@@ -10,6 +10,7 @@ int main(){
     printf("p[0] = %d\n", p[0]);
     printf("p[1] = %d\n", p[1]);
     printf("p[2] = %d\n", p[2]);
+    printf("p[3] = %d\n", p[3]);
     free(p);
     system("pause");
     return 0;
