f2675d71ebcacaebb4935f55b2e2fd19265aad86
AmeyPrakash <aditiaditi737@gmail.com>
2026-08-05 18:02:44 +0530

    renamed the file as it was conflicting with the run command

---
+++ DMA_calloc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a;
+    int *p;
+    p = (int*)calloc(20, sizeof(int));
+    p[0] = 10;
+    p[1] = 20;
+    printf("p[0] = %d\n", p[0]);
+    printf("p[1] = %d\n", p[1]);
+    printf("p[2] = %d\n", p[2]);
+    free(p);
+    system("pause");
+    return 0;
+}
